ee5e8e7651238d32d7f3f3eb04e9fa16cbf4810a
9sarkan <sarkan.9m@gmail.com>
2019-03-27 04:03:47 +0430

    sqlite3 deleted and postgresql configrated

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookmarks',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
