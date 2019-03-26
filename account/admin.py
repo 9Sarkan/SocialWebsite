4c345efa169595fc3797147d19623328c0e69f4d
9sarkan <sarkan.9m@gmail.com>
2019-03-27 03:04:44 +0430

    panel admin created for ChangePasswordToken model

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import ChangePasswordToken
 
-# Register your models here.
+
+@admin.register(ChangePasswordToken)
+class changepassworktokenpanel(admin.ModelAdmin):
+    list_display = ['email', ]
+
+
