3bd9930084d10d5905ac3114c61abad48d4e4205
9sarkan <sarkan.9m@gmail.com>
2019-03-28 21:46:17 +0430

    account app panel admin modified

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-from .models import ChangePasswordToken
+from .models import ChangePasswordToken, NUser
+from django.http import HttpResponse
+from django.core import serializers
 
+
+def export_as_json(ModelAdmin, request, queryset):
+    respose = HttpResponse(content_type='Application/json')
+    serializers.serialize('json', queryset,stream=respose)
+    return respose
+
+export_as_json.short_description = 'export selected item as json'.title()
 
 @admin.register(ChangePasswordToken)
 class changepassworktokenpanel(admin.ModelAdmin):
     list_display = ['email', ]
+    actions = [export_as_json, ]
 
+@admin.register(NUser)
+class nuseradmin(admin.ModelAdmin):
+    list_display = ['user', 'number', ]
+    actions = [export_as_json, ]
 
