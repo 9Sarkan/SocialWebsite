4f630e72715f86662a8d29959d3e0f2dfcc54c46
9sarkan <sarkan.9m@gmail.com>
2019-03-28 22:28:46 +0430

    post, like and comment models created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    slug = models.SlugField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    photo = models.ImageField()
+    date = models.DateTimeField(auto_created=True)
+    comment = models.CharField(null=True, blank=True, max_length=500)
+
+    def __str__(self):
+        return 'Post {0} - user {1}'.format(self.id, self.user.username)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='Likes')
+    date = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='comments', null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    date = models.DateTimeField(auto_now=True)
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return 'Comment : {0}, User {1}'.format(self.id, self.user.username)
+
